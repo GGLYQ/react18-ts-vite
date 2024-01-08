73131115309e1c1fee872b7db1cc1613a8214301
LYQGG <lyq15016261344@163.com>
2024-01-08 11:08:20 +0800

    prove 添加打包运行的基础配置

--- README.md
+++ README.md
@@ -29,6 +29,7 @@ export default {
 - Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
 - Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
 
+```js
 //安装 redux 
 yarn add redux react-redux
 npm i redux react-redux
@@ -76,5 +77,19 @@ yarn add  @ant-design/icons
 若想查看Git版本控制状态及其他常用命令，可以使用git status命令。
 
 若想查看Git日志记录，可以使用git log命令。
+```
+```txt
+(1)vite-plugin-html 是一个 Vite 插件，用于将传统 HTML 文件作为输出文件使用。该插件基于 html-webpack-plugin 插件创建，但是是专门为 Vite 构建的优化工具，不依赖于 webpack。
 
-提示：AI自动生成，仅供参考
+(2)vite-plugin-image-optimizer 是一个 Vite 插件，用于自动优化应用程序中的图像文件。该插件可以帮助开发者减少应用程序的加载时间和带宽使用，从而提高用户体验和网站性能。
+
+(3)vite-plugin-compression是一个基于Vite的插件,用于gzip或Brotli压缩你的资源，从而减少页面的加载时间和网络带宽，提高用户访问速度和体验。
+
+(4)vite-plugin-remove-console事一款生产环境删除所有指定console类型的vite插件。
+
+(5)rollup-plugin-visualizer 是一个用于可视化 Rollup 打包输出文件的插件，它可以帮助开发者分析和优化代码依赖关系，从而提高代码性能和可读性。
+
+(6)vite-svg-loader 是一个 Vite 插件，用于加载和处理 SVG 文件。它可以帮助开发者在应用程序中无缝使用 SVG 图像，并提供了一些额外的功能来优化和定制 SVG 图像的使用。
+
+(7)unplugin-auto-import 是一个 Vite 插件，可以自动将需要的模块或库注入到 JavaScript 或 TypeScript 文件中。该插件可以帮助开发者减少手动导入模块的工作量，并且可以防止由于拼写错误或路径错误导致的编译错误。
+```
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,94 @@
-import { defineConfig } from 'vite'
+import { defineConfig, ConfigEnv, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import path, { resolve } from 'path'
+import glob from 'glob'
+import { createHtmlPlugin } from 'vite-plugin-html'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import viteCompression from 'vite-plugin-compression';
+import removeConsole from "vite-plugin-remove-console";
 
+// 设置不同环境不同命令的入口文件
+function getEntry(moduleName) {
+  const input =
+    moduleName === 'development'
+      ? glob.sync(path.resolve(__dirname, './public', 'index.html'))
+      : {
+        [moduleName]: resolve(__dirname, `./public/${moduleName}/index.html`),
+      }
+  return input
+}
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command, mode }: ConfigEnv) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const moduleName = mode || 'development'
+  const input = getEntry(moduleName)
+  console.log(command, mode);
+  return {
+    // 静态资源基础路径 base: './' || '',
+    base: env.NODE_ENV === 'production' ? './' : '/',
+    plugins: [
+      react(),
+      // 生产环境删除所有指定console类型
+      removeConsole(),
+      // 用于gzip或Brotli压缩你的资源
+      viteCompression(),
+      // 用于将传统 HTML 文件作为输出文件使用
+      createHtmlPlugin({
+        minify: true,
+        pages: [
+          {
+            entry: '../src/main.tsx',
+            filename: 'index.html',
+            template: 'public/index.html',
+            injectOptions: {
+              data: {
+                title: 'index',
+                // injectScript: `<script src="./inject.js"></script>`,
+              },
+            },
+          },]
+      }),
+      // 用于自动优化应用程序中的图像文件
+      ViteImageOptimizer({
+      }),
+    ],
+    // 项目根目录，index.html 所在的目录
+    // 要配置多页面，所以此处更改项目根目录地址，不再是项目根目录
+    // 而是指定的目录下， 以便配置多页面index.html入口
+    root: resolve(__dirname, ''),
+    // 静态资源服务目录地址
+    // 根目录变化，原来的public静态资源目录则需要，指向
+    publicDir: resolve(__dirname, './public'),
+    // 存储缓存文件的目录地址
+    cacheDir: '',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    // 构建目录自动清除
+    emptyOutDir: false,
+    build: {
+      outDir: path.join(__dirname, './dist', moduleName),
+      rollupOptions: {
+        input,
+        output: {
+          chunkFileNames: 'js/[name]-[hash].js',
+          entryFileNames: 'js/[name]-[hash].js',
+          assetFileNames: '[ext]/[name]-[hash].[ext]',
+        },
+      },
+    },
+    define: {
+      __NODE_ENV__: JSON.stringify(env.NODE_ENV),
+      __REACT_TITLE__: JSON.stringify(env.REACT_TITLE)
+    },
+    server: {
+      open: true // 自动打开浏览器
+    },
+    esbuild: {
+      // 移除日志打印及debugger,可在env配置VITE_DROP_CONSOLE
+      drop: env.VITE_DROP_CONSOLE ? ['console', 'debugger'] : []
+    }
+  }
 })
