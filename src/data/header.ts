4a77a2e8de56c14823a0df5f15aad37d19c005e6
LYQGG <lyq15016261344@163.com>
2024-01-13 04:05:06 +0800

    prove 优化头部信息

---
+++ src/data/header.ts
@@ -0,0 +1,25 @@
+let systemTitle = "勘地云"
+let menuList = [
+  {
+    title: "页面1",
+    router: "home/page1"
+  }, {
+    title: "页面2",
+    router: "home/page2"
+  },
+  {
+    title: "页面3",
+    router: "home/page3"
+  }, {
+    title: "页面4",
+    router: "home/page4"
+  }
+]
+let useInfo = {
+  name: "开发测试"
+}
+export {
+  systemTitle,
+  menuList,
+  useInfo
+}
--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React from 'react'
 import './index.scss'
+import { systemTitle, menuList, useInfo } from '@/data/header'
+import type { IObj } from '@/utils/base'
+
 class Header extends React.Component {
   // let {user} = this.props;
-  componentDidMount(){}
-  componentDidUpdate(){}
-  componentWillUnmount(){}
+  componentDidMount() {}
+  componentDidUpdate() {}
+  componentWillUnmount() {}
+  menuClick(item: IObj) {
+    console.log(item)
+  }
   render() {
-    return <div className='App-header'>Header</div>;
+    return (
+      <div className='App-header flex flex-center-between'>
+        <div className='App-header-title'>{systemTitle}</div>
+        <div className='App-header-content flex'>
+          {/* 菜单栏 */}
+          <div className='App-header-menu flex'>
+            {menuList.map((menu) => {
+              return (
+                <div className='App-header-menu-item' onClick={() => this.menuClick(menu)}>
+                  {menu.title}
+                </div>
+              )
+            })}
+          </div>
+          {/* 用户信息 */}
+          <div className='App-header-useinfo'>{useInfo.name}</div>
+        </div>
+      </div>
+    )
   }
 }
 export default Header
