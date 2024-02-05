0936b5cb468f788b5dba57cbd8e2bbafeba8e8b0
LYQGG <lyq15016261344@163.com>
2024-02-05 14:17:52 +0800

    prove 初始化地图服务

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,17 +1,29 @@
-import React, { useEffect } from 'react'
-// import { loadModules } from 'esri-loader'
+import React from 'react'
+import { loadModules } from 'esri-loader'
 import './index.scss'
 
-// interface propType {}
-// function Map(props: propType): React.ReactNode {
-
-function Map(): React.ReactNode {
-  // console.log(props)
-  useEffect(() => {
-  }, [])
-  return (
-    <div className='App-map'>
-    </div>
-  )
+interface PropType {}
+interface StateType {}
+class Map extends React.PureComponent<PropType, StateType> {
+  public esriMap: any
+  constructor(props: PropType) {
+    super(props)
+    this.state = {}
+  }
+  async componentDidMount(): Promise<void> {
+    const [Map, MapView] = await loadModules(['esri/Map', 'esri/views/MapView'], { version: '4.24', css: true })
+    this.esriMap = new Map({
+      basemap: 'streets-vector',
+    })
+    new MapView({
+      map: this.esriMap,
+      container: 'FirstMap',
+      center: [-118.244, 34.052],
+      zoom: 12,
+    })
+  }
+  render(): React.ReactNode {
+    return <div id='FirstMap' className='App-FirstMap'></div>
+  }
 }
 export default Map
